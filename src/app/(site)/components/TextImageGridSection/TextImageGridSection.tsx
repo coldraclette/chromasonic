54bad2d43915f85d1df8ad4ca887203041055020
Timo Tännler <timo.taennler@netnode.ch>
2024-03-29 17:59:14 +0200

    spacing

--- src/app/(site)/components/TextImageGridSection/TextImageGridSection.tsx
+++ src/app/(site)/components/TextImageGridSection/TextImageGridSection.tsx
@@ -17,7 +17,7 @@ export default function TextImageGridSection({
   data,
 }: TextImageGridSectionProps) {
   return (
-    <div className="flex h-full w-full flex-col gap-40 lg:gap-80 bg-black pt-10 pb-40  lg:py-32">
+    <div className="flex h-full w-full flex-col gap-40 lg:gap-80 bg-black pt-10 pb-40 lg:pb-80 lg:py-32">
       {data.map((item, index) => {
         return <GridItem key={index} item={item} />;
       })}
